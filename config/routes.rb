5194aa6aa4efbb76e9f8da97f5e4fa5e37d70e53
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-02-13 10:31:03 +0100

    fin app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-
-  root 'static_pages#home'
-  
   devise_for :moussaillons
 
   resources :ragots
+
+  root 'static_pages#home'
 
 end
